55fd5f624ce9a3fe8f97396c6a86c71642d41f99
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-07-05 16:01:44 +0100

    feat: layout for register and login completed

---
+++ app/src/main/java/com/themavencoder/digipharma_vendor/registration/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.themavencoder.digipharma_vendor.registration.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.themavencoder.digipharma_vendor.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
